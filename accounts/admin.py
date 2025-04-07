1148b055fee2ae4156ddecd7d636ebf1220c11d0
suh124 <suhtjddmlgus124@gmail.com>
2025-04-07 20:05:43 +0900

    FEAT: 관리자 페이지에서 User 관련 조작을 할 수 있도록 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from .models import User
 
 
-admin.site.register(User)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'role']
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'role']
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('역할', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('역할', {'fields': ('role',)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
