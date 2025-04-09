fa7a402ea7fda95c9acee2ca0f716ba8b9cdd7a8
suh124 <suhtjddmlgus124@gmail.com>
2025-04-09 11:53:37 +0900

    FIX: 배포 서버 CORS 설정 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,5 +2,5 @@ from .base import *
 
 ALLOWED_HOSTS = ['10.171.24.201', 'sasaqanda.kro.kr']
 
-CORS_ALLOWED_ORIGINS = ['10.171.24.201:30046', 'sasaqanda.kro.kr:30046']
-CSRF_TRUSTED_ORIGINS = ['10.171.24.201:30046', 'sasaqanda.kro.kr:30046']
+CORS_ALLOWED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046']
+CSRF_TRUSTED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046']
