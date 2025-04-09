31ef5793d568c2418c015056dfabf67503b722da
suh124 <suhtjddmlgus124@gmail.com>
2025-04-09 11:50:39 +0900

    FEAT: 배포용 서버 CORS 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = ['10.171.24.201', 'sasaqanda.kro.kr']
+
+CORS_ALLOWED_ORIGINS = ['10.171.24.201:30046', 'sasaqanda.kro.kr:30046']
+CSRF_TRUSTED_ORIGINS = ['10.171.24.201:30046', 'sasaqanda.kro.kr:30046']
