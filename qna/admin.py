c04052b0dc329533db12cf24a1bf04ed766ec451
suh124 <suhtjddmlgus124@gmail.com>
2025-04-09 13:35:56 +0900

    FEAT: Tag 및 Tag 카테고리 트리 추가

--- qna/admin.py
+++ qna/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models.question import Question, Category
+from .models.question import Question, Category, Tag, TagCategory
 
 
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.register(TagCategory)
+
+
+admin.site.register(Tag)
--- qna/models/question.py
+++ qna/models/question.py
@@ -18,11 +18,28 @@ class Category(models.Model):
         return f"{self.name} ({'>'.join([i['name'] for i in self.get_breadcrum()])})"
         
 
+class TagCategory(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subcategories')
+
+    def get_breadcrum(self):
+        ret = [{'id': self.id, 'name': self.name}]
+        parent = self.parent
+        while parent:
+            ret.insert(0, {'id': parent.id, 'name': parent.name})
+            parent = parent.parent
+        return ret
+    
+    def __str__(self):
+        return f"{self.name} ({'>'.join([i['name'] for i in self.get_breadcrum()])})"
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    tag_category = models.ForeignKey(TagCategory, on_delete=models.PROTECT, related_name='tags')
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} ({self.tag_category.name})"
     
 
 class Question(models.Model):
