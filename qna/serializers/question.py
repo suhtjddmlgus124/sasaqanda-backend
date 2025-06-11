c6f0ea4fd72b6ddccf86ec16318c4d02559838d8
suh124 <suhtjddmlgus124@gmail.com>
2025-06-11 22:13:36 +0900

    FIX: 파일 확장자 검사 기능 수정

--- qna/serializers/question.py
+++ qna/serializers/question.py
@@ -13,7 +13,7 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ['id', 'subject_category', 'image', 'content', 'tags']
 
     def validate_image(self, value):
-        ext = os.path.splitext(value.name)[-1].lower()
+        ext = os.path.splitext(value.name)[1].lower()
         if ext not in ['.png', '.jpg', '.jpeg']:
             raise serializers.ValidationError('PNG, JPG, JPEG 파일만 업로드할 수 있습니다')
         return value
@@ -23,7 +23,7 @@ class QuestionImageSerializer(serializers.Serializer):
     image = serializers.ImageField()
 
     def validate_image(self, value):
-        ext = os.path.splitext(value.name)[-1].lower()
+        ext = os.path.splitext(value.name)[1].lower()
         if ext not in ['.png', '.jpg', '.jpeg']:
             raise serializers.ValidationError('PNG, JPG, JPEG 파일만 업로드할 수 있습니다')
         return value
