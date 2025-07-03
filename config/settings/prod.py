91975c1c5b84fdb791c52a235a367662e300e704
suh124 <suhtjddmlgus124@gmail.com>
2025-07-03 11:20:18 +0900

    FEAT: 허용 URL 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['10.171.24.201', 'sasaqanda.kro.kr']
+ALLOWED_HOSTS = ['10.171.24.201', 'sasaqanda.kro.kr', 'ai.sasahs.site']
 
-CORS_ALLOWED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046']
-CSRF_TRUSTED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046']
+CORS_ALLOWED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046', 'http://ai.sasahs.site:30046']
+CSRF_TRUSTED_ORIGINS = ['http://10.171.24.201:30046', 'http://sasaqanda.kro.kr:30046', 'http://ai.sasahs.site:30046']
